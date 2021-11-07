07e073735b9d0a67da237e01b7b89f9cc19942e4
Marc Robert Wong <marcrobertwong@gmail.com>
2021-11-06 22:34:18 -0700

    QWERTY version.

--- keyboards/le_chiffre/keymaps/seniply/keymap.c
+++ keyboards/le_chiffre/keymaps/seniply/keymap.c
@@ -24,28 +24,32 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT(
-    KC_Q, KC_W, KC_F,  KC_P,    KC_B,   KC_MPLY, KC_J,   KC_L,      KC_U,    KC_Y,   KC_QUOT,
-    KC_A, KC_R, KC_S,  KC_T,    KC_G,            KC_M,   KC_N,      KC_E,    KC_I,   KC_O,
-    KC_Z, KC_X, KC_C,  KC_D,    KC_V,            KC_K,   KC_H,      KC_COMM, KC_DOT, KC_SLSH,
-                MO(EXTEND),     KC_LSFT,         KC_SPC, MO(SYMBOL)
+    KC_Q, KC_W, KC_E,  KC_R,  KC_T, KC_MPLY, KC_Y, KC_U, KC_I,    KC_O,   KC_P,
+    KC_A, KC_S, KC_D,  KC_F,  KC_G,          KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,
+    KC_Z, KC_X, KC_C,  KC_V,  KC_B,          KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
+    // THUMB ROW
+    MO(EXTEND), KC_LSFT, KC_SPC, MO(SYMBOL)
   ),
   [EXTEND] = LAYOUT(
     KC_ESC,        KC_MUTE,       KC_VOLD,       KC_VOLU,       KC_NO,         KC_TRNS, KC_PGUP, KC_HOME, KC_UP,         KC_END,        KC_CAPS,
     OSM(MOD_LALT), OSM(MOD_LGUI), OSM(MOD_LSFT), OSM(MOD_LCTL), OSM(MOD_RALT),          KC_PGDN, KC_LEFT, KC_DOWN,       KC_RGHT,       KC_DEL,
     KC_TAB,        KC_MRWD,       KC_MPLY,       KC_MFFD,       KC_NO,                  KC_ENT,  KC_BSPC, OSM(MOD_RSFT), OSM(MOD_RCTL), KC_NO,
-                                  KC_TRNS,       KC_TRNS,                               KC_TRNS, KC_TRNS
+    // THUMB ROW
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
   [FUNCTION] = LAYOUT(
     KC_TILD,       KC_GRV,        KC_BSLS,       KC_PIPE,       KC_NO,         KC_TRNS, KC_F12,  KC_F7,  KC_F8, KC_F9, KC_NO,
     OSM(MOD_LALT), OSM(MOD_LGUI), OSM(MOD_LSFT), OSM(MOD_LCTL), OSM(MOD_RALT),          KC_F11,  KC_F4,  KC_F5, KC_F6, KC_NO,
     RESET,         KC_NO,         KC_NO,         KC_NO,         KC_NO,                  KC_F10,  KC_F1,  KC_F2, KC_F3, KC_NO,
-                                  KC_TRNS,       KC_TRNS,                               KC_TRNS, KC_TRNS
+    // THUMB ROW
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
   [SYMBOL] = LAYOUT(
     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_TRNS, KC_EQL,  KC_7,   KC_8, KC_9, KC_PLUS,
-    KC_SCLN, KC_COLN, KC_LCBR, KC_LPRN, KC_LBRC,          KC_8,    KC_4,   KC_5, KC_6, KC_MINS,
+    KC_QUOT, KC_COLN, KC_LCBR, KC_LPRN, KC_LBRC,          KC_ASTR, KC_4,   KC_5, KC_6, KC_MINS,
     KC_CIRC, KC_AMPR, KC_RCBR, KC_RPRN, KC_RBRC,          KC_0,    KC_1,   KC_2, KC_3, KC_SLSH,
-                      KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS
+    // THUMB ROW
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 };
 

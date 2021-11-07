cc2f015549d57ad46d692aaaa4a9a8cd5e6f59ac
Marc Robert Wong <marcrobertwong@gmail.com>
2021-11-06 23:47:34 -0700

    Switch to usermode oneshot implementation (callum).

--- keyboards/le_chiffre/keymaps/seniply/keymap.c
+++ keyboards/le_chiffre/keymaps/seniply/keymap.c
@@ -15,11 +15,21 @@
  */
 #include QMK_KEYBOARD_H
 
+#include "oneshot.h"
+
 enum layers {
   BASE,
   EXTEND,
   SYMBOL,
   FUNCTION,
+};
+
+enum keycodes {
+  // Custom oneshot mod implementation with no timers.
+  OS_SHFT = SAFE_RANGE,
+  OS_CTRL,
+  OS_ALT,
+  OS_GUI,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -31,9 +41,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     MO(EXTEND), KC_LSFT, KC_SPC, MO(SYMBOL)
   ),
   [EXTEND] = LAYOUT(
-    KC_ESC,        KC_MUTE,       KC_VOLD,       KC_VOLU,       KC_NO, KC_TRNS, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_CAPS,
-    OSM(MOD_LSFT), OSM(MOD_LCTL), OSM(MOD_LGUI), OSM(MOD_LALT), KC_NO,          KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,
-    KC_TAB,        KC_MRWD,       KC_MPLY,       KC_MFFD,       KC_NO,          KC_ENT,  KC_BSPC, KC_NO,   KC_NO,   KC_NO,
+    KC_ESC,  KC_MUTE, KC_VOLD, KC_VOLU, KC_NO, KC_TRNS, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_CAPS,
+    OS_SHFT, OS_CTRL, OS_GUI,  OS_ALT,  KC_NO,          KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,
+    KC_TAB,  KC_MRWD, KC_MPLY, KC_MFFD, KC_NO,          KC_ENT,  KC_BSPC, KC_NO,   KC_NO,   KC_NO,
     // THUMB ROW
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
@@ -45,13 +55,64 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
   [FUNCTION] = LAYOUT(
-    KC_GRV,        KC_TILD,       KC_BSLS,       KC_PIPE,       KC_NO,  KC_TRNS, KC_F12, KC_F7, KC_F8, KC_F9, RESET,
-    OSM(MOD_LSFT), OSM(MOD_LCTL), OSM(MOD_LGUI), OSM(MOD_LALT), KC_NO,           KC_F11, KC_F4, KC_F5, KC_F6, RGB_TOG,
-    KC_NO,         KC_NO,         KC_NO,         KC_NO,         KC_NO,           KC_F10, KC_F1, KC_F2, KC_F3, KC_NO,
+    KC_GRV,  KC_TILD, KC_BSLS, KC_PIPE, KC_UNDS,  KC_TRNS, KC_F12, KC_F7, KC_F8, KC_F9, RESET,
+    OS_SHFT, OS_CTRL, OS_GUI,  OS_ALT,  KC_NO,             KC_F11, KC_F4, KC_F5, KC_F6, RGB_TOG,
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,             KC_F10, KC_F1, KC_F2, KC_F3, KC_NO,
     // THUMB ROW
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 };
+
+bool is_oneshot_cancel_key(uint16_t keycode) {
+    switch (keycode) {
+    case MO(EXTEND):
+    case MO(SYMBOL):
+        return true;
+    default:
+        return false;
+    }
+}
+
+bool is_oneshot_ignored_key(uint16_t keycode) {
+    switch (keycode) {
+    case MO(EXTEND):
+    case MO(SYMBOL):
+    case KC_LSFT:
+    case OS_SHFT:
+    case OS_CTRL:
+    case OS_ALT:
+    case OS_GUI:
+        return true;
+    default:
+        return false;
+    }
+}
+
+oneshot_state os_shft_state = os_up_unqueued;
+oneshot_state os_ctrl_state = os_up_unqueued;
+oneshot_state os_alt_state = os_up_unqueued;
+oneshot_state os_gui_state = os_up_unqueued;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    update_oneshot(
+        &os_shft_state, KC_LSFT, OS_SHFT,
+        keycode, record
+    );
+    update_oneshot(
+        &os_ctrl_state, KC_LCTL, OS_CTRL,
+        keycode, record
+    );
+    update_oneshot(
+        &os_alt_state, KC_LALT, OS_ALT,
+        keycode, record
+    );
+    update_oneshot(
+        &os_gui_state, KC_LGUI, OS_GUI,
+        keycode, record
+    );
+
+    return true;
+}
 
 layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, EXTEND, SYMBOL, FUNCTION);

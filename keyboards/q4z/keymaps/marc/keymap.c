0eb2b87ceed2bf7e652950cc1b1019e2574617f3
Marc Robert Wong <marcrobertwong@gmail.com>
2022-04-13 17:47:02 -0700

    Swap Shift and Space.

--- keyboards/q4z/keymaps/marc/keymap.c
+++ keyboards/q4z/keymaps/marc/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
    KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN,
    KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,
-   KC_NO, KC_NO, MO(NAV), KC_LSFT, KC_SPC, MO(SYM), TG(GAME), KC_SLSH
+   KC_NO, KC_NO, MO(NAV), KC_SPC, KC_LSFT, MO(SYM), TG(GAME), KC_SLSH
   ),
 
   [SYM] = LAYOUT(

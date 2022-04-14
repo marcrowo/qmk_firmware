82094ec9114ae97c2fbdf87042853974cd447a2d
Marc Robert Wong <marcrobertwong@gmail.com>
2022-04-13 17:43:00 -0700

    Swap Shift and Space.

--- keyboards/le_chiffre/keymaps/marc/keymap.c
+++ keyboards/le_chiffre/keymaps/marc/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_A, KC_S, KC_D,  KC_F,  KC_G,        KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,
     KC_Z, KC_X, KC_C,  KC_V,  KC_B,        KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
     // THUMB ROW
-    MO(NAV), KC_LSFT, KC_SPC, MO(SYM)
+    MO(NAV), KC_SPC, KC_LSFT, MO(SYM)
   ),
   [SYM] = LAYOUT(
     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_TRNS, KC_MINS, KC_PLUS, KC_UNDS, KC_EQL,  KC_ASTR,
